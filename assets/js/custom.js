25524812e4230ae77b11bea76a768a6b7140ef27
Baptiste Sapa <contact@baptistesapa.fr>
2020-02-26 15:56:42 +0100

    Fonctionne sur Firefox et Chrome

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,19 @@
-// chrome.browserAction.onClicked.addListener(function () { $
-//     chrome.tab.create({'url':"chrome://newtab"})
-// });
+var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 
-console.log("testouille");
+if (isFirefox) {
+    browser.browserAction.onClicked.addListener(function () {
+
+        alert("On Firefox");
+        var creating = browser.tabs.create({
+            url: "./../template/index.html"
+        }); 
+        creating.then(onCreated, onError);
+    });
+} else {
+    chrome.browserAction.onClicked.addListener(function () {
+        chrome.tabs.create({
+            'url': "chrome://newtab"
+        })
+        alert("On Chrome");
+    });
+}
