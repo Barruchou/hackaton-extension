9258633001ab16d0fce617b24bf8d8f4079dad48
Felix Barruche <felix.barruche@gmail.com>
2020-02-27 12:21:11 +0100

    add feature random, add animation and improce css

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,1 +1,41 @@
-console.log("hello world");
+window.addEventListener("DOMContentLoaded", function(event) {
+    getJSON("./../assets/json/data.json");
+});
+
+function getJSON(url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        useJSON(this.responseText)
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+}
+
+function getSVG(url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        useSVG(this.responseText)
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+}
+
+function useJSON(json) {
+    var data = JSON.parse(json);
+    console.log(data);
+    var randomItem = getRandomItemFromArray(data);
+    var fact = document.getElementById('fact');
+    var tip = document.getElementById('tip');
+    fact.innerText = randomItem["fact"];
+    tip.innerText = randomItem["tip"];
+    getSVG(randomItem["image"]);
+}
+
+function useSVG(svg) {
+    var animation = document.getElementById('animation');
+    animation.innerHTML = svg;
+}
+
+function getRandomItemFromArray(array){
+    return array[Math.floor(Math.random() * array.length)];
+}
